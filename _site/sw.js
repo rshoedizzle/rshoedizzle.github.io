3d3c9a8acdfe344ba5c9cc93cc554b59b6569ca5
rshoedizzle <rschu488@gmail.com>
2023-12-13 20:44:59 -0800

    trying to update favicon attempt 4

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1702527418';
+const cacheName = 'chirpy-1702528668';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
